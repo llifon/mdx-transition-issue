55e59193550b569a3cf4ca1b73850673d8e7dda0
Llifon Osian Jones <llifon.jones@beerobotics.com>
2017-10-12 11:03:17 +0100

    First commit demonstrating the issue

---
+++ TransitionOpacityIssue/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TransitionOpacityIssue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
